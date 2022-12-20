e33233987745f600d5118afe2a13ab19bc241bf0
miloszczapla <miloszczapla@gmail.com>
2022-12-20 09:02:57 +0100

    Resolved Pick and Omit easy challenges

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,18 @@
-type MyOmit<T, K> = any
+type MyOmit<T, K extends keyof T> = {
+  [Key in Exclude<keyof T, K>]: T[Key]
+}
+
+type MyObj = { a: 'b'; b: 'b'; c: 'c' }
+type MyObjKeys = keyof MyObj
+
+type MyExclude2<Union, Keys> = Union extends Keys ? never : Union
+
+type Y = MyExclude2<keyof MyObj, 'a' | 'b'>
+
+type Identity<T> = T
+
+type MyRecord<OurObject, ToExcludeUnion> = {
+  [Key in MyExclude2<keyof OurObject, ToExcludeUnion>]: OurObject[Key]
+}
+
+type MR1 = MyRecord<{ foo: string; bar: number }, 'foo'>
