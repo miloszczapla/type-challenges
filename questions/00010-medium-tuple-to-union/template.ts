03e80209746d3b334cbffdd1999d807d778cd290
miloszczapla <miloszczapla@gmail.com>
2023-01-03 15:14:33 +0100

    Solve tuple-to-union

--- questions/00010-medium-tuple-to-union/template.ts
+++ questions/00010-medium-tuple-to-union/template.ts
@@ -1,1 +1,10 @@
-type TupleToUnion<T> = any
+type TupleToUnion<T extends Array<any>> = T extends [infer First, ...infer Rest]
+  ? First | TupleToUnion<Rest>
+  : never
+//   ? A | G
+//   :
+
+type ab = TupleToUnion<[string, '222', 2]>
+type cf = TupleToUnion<[string]>
+
+type ag = TupleToUnion<['a']>
