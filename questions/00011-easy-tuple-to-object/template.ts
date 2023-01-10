2d5ed5aaba3ccc715e52eebe8bfec69695b737b9
miloszczapla <miloszczapla@gmail.com>
2023-01-10 09:23:50 +0100

    Solve solve-tuple-to-object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,8 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (number | string)[]> = {
+  [K in T[number]]: K
+}
+const y = ['a', 2] as const
+type C = typeof y
+type U = C[number]
+
+type F = TupleToObject<typeof y>
